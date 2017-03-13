ad563f0d0bac248ffb4b9650393beec4950324ef
Edward Lance Lorilla <edwardlorilla1986@gmail.com>
2017-03-14 00:21:56 +0800

    isplaying product

---
+++ resources/assets/js/shoppingCartState.js
@@ -0,0 +1,38 @@
+// shopping cart state
+export default {
+    data: {
+        cart:[
+
+            ]
+},
+add (product) {
+    var found = _.find(this.data.cart, ['id', product.id])
+    if(typeof found != 'object') {
+        this.data.cart.push({
+            id: product.id,
+            title: product.product,
+            price: product.price,
+            image: product.image.file,
+            qty: 1
+        })
+    }
+},
+inc (product) {
+    var found = _.find(this.data.cart, ['id', product.id])
+    if(typeof found == 'object') {
+        var index = _.indexOf(this.data.cart, found)
+        this.data.cart[index].qty++
+    }
+},
+dec (product) {
+    var found = _.find(this.data.cart, ['id', product.id])
+    if(typeof found == 'object') {
+        var index = _.indexOf(this.data.cart, found)
+        if(this.data.cart[index].qty == 1) {
+            this.data.cart.$remove(found)
+        } else {
+            this.data.cart[index].qty--
+        }
+    }
+}
+}
